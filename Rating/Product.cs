751961d9a61284437f1a99a4254faea41c2ef689
Alexandre Konioukhov <alexan1@gmail.com>
2020-04-18 17:43:47 -0400

    ResetDemo

---
+++ Rating/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Rating
+{
+    class Products
+    {
+        public class ProductRating
+        {
+            [BsonElement("Rating")]
+            public int Rating { get; set; }
+        }
+
+        public class Product
+        {
+            [BsonId]
+            public ObjectId Id { get; set; }
+
+
+            [BsonElement("PLU")]
+            public int PLU { get; set; }
+
+            [BsonElement("Description")]
+            public string Description { get; set; }
+
+            [BsonElement("AverageRating")]
+            public Double AvgRating { get; set; }
+
+
+            [BsonElement("Ratings")]
+            public List<ProductRating> Ratings { get; set; }
+        }
+    }
+}
--- Rating/RatingApi.cs
+++ Rating/RatingApi.cs
@@ -8,6 +8,12 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using static Rating.Products;
+using System;
 
 namespace Rating
 {
@@ -66,6 +72,60 @@ namespace Rating
             //ratings.r
 
             return new OkObjectResult(rate);
+        }
+
+        [FunctionName("ResetDemo")]
+        public static async Task<IActionResult> RunGet([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            try
+            {
+
+                var client = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDBAtlasConnectionString"));
+                var database = client.GetDatabase("MongoOnlineGrocery");
+                var collection = database.GetCollection<Product>("inventory");
+                //We could also just drop the collection
+                await collection.DeleteManyAsync(new BsonDocument { });
+                await collection.Indexes.DropAllAsync();
+
+                //Create an index on the PLU, this will also enforce uniqueness
+                IndexKeysDefinition<Product> keys = "{ PLU: 1 }";
+                var indexModel = new CreateIndexModel<Product>(keys);
+                await collection.Indexes.CreateOneAsync(indexModel);
+
+                //Create a default rating of 3 for new products
+                var MyRating = new ProductRating();
+                MyRating.Rating = 3;
+                var DefaultProductRating = new List<ProductRating>();
+                DefaultProductRating.Add(MyRating);
+
+                //Define some sample objects
+                var Bananas = new Product
+                {
+                    Id = new ObjectId(),
+                    PLU = 4011,
+                    Description = "Bananas",
+                    Ratings = DefaultProductRating
+                };
+                var Apples = new Product
+                {
+                    Id = new ObjectId(),
+                    PLU = 3283,
+                    Description = "Apples",
+                    Ratings = DefaultProductRating
+                };
+
+                //MongoDB makes it easy to go from object to database with no ORM layer
+                await collection.InsertOneAsync(Bananas);
+                await collection.InsertOneAsync(Apples);
+
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult("Error refreshing demo - " + e.Message);
+
+            }
+
+            return (ActionResult)new OkObjectResult("Refreshed Demo database");
         }
     }
 }

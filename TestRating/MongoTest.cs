a997e9d7acda1b2019cbed896405edd58753c2f8
Alexandre Konioukhov <alexan1@gmail.com>
2021-04-06 14:38:57 -0400

    unitest

---
+++ TestRating/MongoTest.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoMusic.API.Helpers;
+using System;
+
+namespace TestRating
+{
+    [TestClass]
+    public class MongoTest
+    {
+        //private readonly MongoClient _mongoClient;
+
+        //public MongoTest(MongoClient mongoClient)
+        //{
+        //    _mongoClient = mongoClient;
+        //}
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var result = true;
+            var settings = Environment.GetEnvironmentVariable("MongoDBAtlasConnectionString");
+            var settings1 = "mongodb+srv://alexan1:<passwod>.c0dsb.azure.mongodb.net?retryWrites=true&w=majority";
+            var client = new MongoClient(settings1);
+            var database = client.GetDatabase("People");
+            database.RunCommand((Command<BsonDocument>)"{ping:1}");
+
+            Assert.IsTrue(result);
+            //Assert.AreEqual(settings, settings1);
+
+        }
+    }
+}
